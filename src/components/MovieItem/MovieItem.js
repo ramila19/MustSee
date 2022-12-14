5f98333bb1d9c046af3ca1456676f0c67046022b
unknown <ramilahsynva@gmail.com>
2022-12-14 12:29:22 +0400

    ramcosh

--- src/components/MovieItem/MovieItem.js
+++ src/components/MovieItem/MovieItem.js
@@ -3,13 +3,13 @@ import './MovieItem.css';
 
 class MovieItem extends Component {
     render() {
-        const { title, year, poster } = this.props;
+        const { Title, Year, Poster, imdbID, Plot } = this.props;        
         return (
             <article className="movie-item">
-                <img className="movie-item__poster" src={poster} alt={title} />
+                <img className="movie-item__poster" src={Poster} alt={Title} />
                 <div className="movie-item__info">
-                    <h3 className="movie-item__title">{title}&nbsp;({year})</h3>
-                    <button type="button" className="movie-item__add-button">Добавить в список</button>
+                    <h3 className="movie-item__title">{Title}&nbsp;({Year})</h3>
+                    <button type="button" className="movie-item__add-button" onClick = {() => this.props.addToList(imdbID)}>Add to the list</button>
                 </div>
             </article>
         );
--- src/components/SearchBox/SearchBox.js
+++ src/components/SearchBox/SearchBox.js
@@ -2,40 +2,31 @@ import React, { Component } from 'react';
 import './SearchBox.css';
 
 class SearchBox extends Component {
-    state = {
-        searchLine: ''
-    }
-    searchLineChangeHandler = (e) => {
-        this.setState({ searchLine: e.target.value });
-    }
-    searchBoxSubmitHandler = (e) => {
-        e.preventDefault();
-    }
-    render() {
-        const { searchLine } = this.state;
 
+    render() {
         return (
             <div className="search-box">
-                <form className="search-box__form" onSubmit={this.searchBoxSubmitHandler}>
-                    <label className="search-box__form-label">
-                        Искать фильм по названию:
-                        <input
-                            value={searchLine}
-                            type="text"
-                            className="search-box__form-input"
-                            placeholder="Например, Shawshank Redemption"
-                            onChange={this.searchLineChangeHandler}
-                        />
-                    </label>
-                    <button
-                        type="submit"
-                        className="search-box__form-submit"
-                        disabled={!searchLine}
-                    >
-                        Искать
-                    </button>
-                </form>
-            </div>
+            <form className="search-box__form" onSubmit={(e) => this.props.movieRequest(e)}>
+                <label className="search-box__form-label">
+                     Search movies by title:
+                    <input
+                        onChange = {(e) => this.props.changeMovies(e)}
+                        name = 'nameMuvei'
+                        value={this.props.searchMovies}
+                        type="text"
+                        className="search-box__form-input"
+                        placeholder="For example, Shawshank Redemption"                          
+                    />
+                </label>
+                <button
+                    type="submit"
+                    className="search-box__form-submit"
+                    disabled={!this.props.searchMovies}
+                >
+                    Search
+                </button>
+            </form>
+        </div>
         );
     }
 }
